8f2d24a599f97372a43f6d344aa9b989d4b6ce54
ZeHolyQofPower <adamstuff@outlook.com>
2021-10-26 14:39:33 -0400

    Blank main compiles correctly new. Makefile needed to be radically fixed.

--- InputFileGenerator.cpp
+++ InputFileGenerator.cpp
@@ -1,6 +1,9 @@
 /* Copyright 2021 Adam Gazdecki
  */
 #include <InputFileGenerator.h>
+using std::cout;
+using std::cin;
+using std::endl;
 /* The purpose of this file (and its header) are to generate a file.
  * This file will be an ASCII file of random, unsorted, floating-point numbers.
  * Their range should be: [-100, 100]
@@ -10,4 +13,9 @@
  * This should be able to generate 100 different input files, per quantity.
  * ZERO functions should be touched by gazdecki_adam_QuickSort files.
  */
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
+
 // ~End of File.
--- InputFileGenerator.h
+++ InputFileGenerator.h
@@ -3,5 +3,8 @@
 #ifndef INPUTFILEGENERATOR_H_
 #define INPUTFILEGENERATOR_H_
 
+#include <iostream>
+// Used for debugging output.
+
 #endif  // INPUTFILEGENERATOR_H_
 // ~End of File.
