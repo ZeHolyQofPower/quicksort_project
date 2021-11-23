ef52ba162beaadb8bac167e3e2e172b4078edd22
ZeHolyQofPower <adamstuff@outlook.com>
2021-11-23 15:00:58 -0500

    Started basic inclusion and wrote instructions.

--- gazdecki_adam_QuickSort.cpp
+++ gazdecki_adam_QuickSort.cpp
@@ -1,5 +1,19 @@
 /* Copyright 2021 Adam Gazdecki
  */
+/* The purpose of this file is to read in, sort files, and give time taken.
+ * It will use QuickSort and a pivot of median-of-three to sort.
+ * It will read in existing files from command line.
+ * The input files will be delimited by spaces and vary in length.
+ * It will not edit the input files.
+ * It will create a new file to write sorted numbers to.
+ * The name of these new sorted files will come from command line input.
+ * The time taken to sort, in milliseconds alongside with the quantity of-
+ * -numbers will be written to a specific existing file.
+ */
 #include <gazdecki_adam_QuickSort.h>
-#include <InputFileGenerator.h>
+// #include <InputFileGenerator.h>
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
 // ~End of File.
--- gazdecki_adam_QuickSort.h
+++ gazdecki_adam_QuickSort.h
@@ -3,5 +3,10 @@
 #ifndef GAZDECKI_ADAM_QUICKSORT_H_
 #define GAZDECKI_ADAM_QUICKSORT_H_
 
+#include <iostream> // Used for debugging output.
+using std::cout;
+using std::cin;
+using std::endl;
+
 #endif  // GAZDECKI_ADAM_QUICKSORT_H_
 // ~End of File.
